29d54ad41120f4e2adcd71b33820502e2ca822e7
David A York <medmatix@gmail.com>
2015-11-15 10:14:18 -0500

    Report Setup
    Intial Blocking out of Report

--- Development_Code.R
+++ Development_Code.R
@@ -1,5 +1,19 @@
 ## Development Code for PML Project
 
-#load required libraries and functions
+# load required libraries and functions
 library("caret")
+
+# fetch Training and Testing data
+fileUrlTrain <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
+fileUrlTest <-"https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+download.file(fileUrlTrain, destfile = "train.csv")
+dateTrainDownloaded <- date()
+
+# save the download dates for reference
+download.file(fileUrlTest, destfile = "test.csv")
+dateTestDownloaded <- date()
+
+# read in the data files for training and testing
+trainingData <- read.csv("train.csv")
+testingData <- read.csv("teat.csv")
 
